59d8300769f193d30efecd2895ba1514476bdea4
PBetzler <phil.betzler@gmx.de>
2024-11-14 12:22:39 +0100

    WIP: changed path determination methodic

--- fuzz-tests/fuzz_encrypt_decrypt.cpp
+++ fuzz-tests/fuzz_encrypt_decrypt.cpp
@@ -13,9 +13,13 @@ extern "C" {
   #include "libutils.h"
 }
 
+static const std::filesystem::path TEST_OUTPUT_DIR = "test-output";
 // One-time initialization tasks
 FUZZ_TEST_SETUP() {
-  // No specific one-time setup required for this fuzz test
+  if (!std::filesystem::exists(TEST_OUTPUT_DIR)) {
+        // Directory does not exist, create it
+        std::filesystem::create_directory(TEST_OUTPUT_DIR);
+    }
 }
 
 // Entry point for the fuzzing harness
@@ -26,11 +30,13 @@ FUZZ_TEST(const uint8_t *data, size_t size) {
   std::string unencrypted_file_name = fdp.ConsumeBytesAsString(fdp.ConsumeIntegralInRange(0, 100));
   std::string encrypted_file_name = fdp.ConsumeBytesAsString(fdp.ConsumeIntegralInRange(0, 100));
 
-  std::string dir = "test-output";
-  unencrypted_file_name = dir / unencrypted_file_name;
-  encrypted_file_name = dir /  encrypted_file_name;
+  std::filesystem::path unencrypted_file_full_path = TEST_OUTPUT_DIR / unencrypted_file_name;
+  std::filesystem::path encrypted_file_full_path = TEST_OUTPUT_DIR /  encrypted_file_name;
+
+  // std::cerr << "Unencrypted file name: " << unencrypted_file_full_path<<std::endl;
+  // std::cerr << "Encrypted file name: " << encrypted_file_full_path<<std::endl;
   std::fstream myFile;
-  myFile.open(unencrypted_file_name, std::ios::out);
+  myFile.open(unencrypted_file_full_path, std::ios::out);
 
   if (!myFile) {
       return;
@@ -40,10 +46,10 @@ FUZZ_TEST(const uint8_t *data, size_t size) {
   myFile.close();
 
 
-  encrypt_file(password.c_str(), unencrypted_file_name.c_str(), encrypted_file_name.c_str());
-  decrypt_file(password.c_str(), encrypted_file_name.c_str(), NULL);
+  encrypt_file(password.c_str(), unencrypted_file_full_path.c_str(), encrypted_file_name.c_str());
+  decrypt_file(password.c_str(), encrypted_file_full_path.c_str(), NULL);
 
-  remove(unencrypted_file_name.c_str());
-  remove(encrypted_file_name.c_str());
+  remove(unencrypted_file_full_path.c_str());
+  remove(encrypted_file_full_path.c_str());
 
 }
