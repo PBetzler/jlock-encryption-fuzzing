f2ef90c28786ee1d4cbc1e5438a8d0896bfc8ac4
PBetzler <phil.betzler@gmx.de>
2024-11-14 13:16:46 +0100

    Cleaned up creation and deletion of tmp files for test

--- fuzz-tests/fuzz_encrypt_decrypt.cpp
+++ fuzz-tests/fuzz_encrypt_decrypt.cpp
@@ -17,18 +17,35 @@ static const std::filesystem::path TEST_OUTPUT_DIR = "test-output";
 // One-time initialization tasks
 FUZZ_TEST_SETUP() {
   if (!std::filesystem::exists(TEST_OUTPUT_DIR)) {
-        // Directory does not exist, create it
-        std::filesystem::create_directory(TEST_OUTPUT_DIR);
-    }
+      // Directory does not exist, create it
+      if (!std::filesystem::create_directory(TEST_OUTPUT_DIR)) {
+          std::cerr << "Test dir did not exist and failed to create it. Dir was supposed to be: " << TEST_OUTPUT_DIR<<std::endl;
+      }
+  }
+}
+
+void deleteFile(const char* fileToDelete) {
+  try {
+    if (!std::filesystem::remove(fileToDelete))
+      std::cerr << "Did not find file to delete: " << fileToDelete<<std::endl;
+
+  }
+  catch(const std::filesystem::filesystem_error& err) {
+     std::cout << "Filesystem error deleting file: " << err.what() << '\n';
+  }
 }
 
 // Entry point for the fuzzing harness
 FUZZ_TEST(const uint8_t *data, size_t size) {
-  // Initialize FuzzedDataProvider
+  // Initialize FuzzedDataProvider // Directory does not exist, create it
   FuzzedDataProvider fdp(data, size);
   std::string password = fdp.ConsumeBytesAsString(fdp.ConsumeIntegralInRange(0, 512));
   std::string unencrypted_file_name = fdp.ConsumeBytesAsString(fdp.ConsumeIntegralInRange(0, 100));
   std::string encrypted_file_name = fdp.ConsumeBytesAsString(fdp.ConsumeIntegralInRange(0, 100));
+  char* re_unencrypted_file_full_path;
+
+  // std::string unencrypted_file_name = "unencrypted_file_name";
+  // std::string encrypted_file_name = "encrypted_file_name";
 
   std::filesystem::path unencrypted_file_full_path = TEST_OUTPUT_DIR / unencrypted_file_name;
   std::filesystem::path encrypted_file_full_path = TEST_OUTPUT_DIR /  encrypted_file_name;
@@ -45,11 +62,20 @@ FUZZ_TEST(const uint8_t *data, size_t size) {
   myFile << fdp.ConsumeRemainingBytesAsString() << std::endl;
   myFile.close();
 
+  myFile.open(encrypted_file_full_path, std::ios::out);
+
+  if (!myFile) {
+      return;
+  }
+  myFile.close();
+
 
   encrypt_file(password.c_str(), unencrypted_file_full_path.c_str(), encrypted_file_name.c_str());
-  decrypt_file(password.c_str(), encrypted_file_full_path.c_str(), NULL);
+  decrypt_file(password.c_str(), encrypted_file_full_path.c_str(), re_unencrypted_file_full_path);
 
-  remove(unencrypted_file_full_path.c_str());
-  remove(encrypted_file_full_path.c_str());
-
+  deleteFile(unencrypted_file_full_path.c_str());  
+  deleteFile(encrypted_file_full_path.c_str());
+  //deleteFile(re_unencrypted_file_full_path);
 }
+
+
